9b27d9cf2793f21e7bc8665ec60c09bbb6643820
ZingerEngineer <ramyisez@gmail.com>
2023-09-07 18:44:57 +0300

    created login/register views

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,4 +1,33 @@
-interface LoginProps {}
+import axios from 'axios'
+import LoginSignupForm from '../components/LoginSignupForm'
+import LoginSignupFormData from '../interfaces/LoginSignupFormData'
 
-const Login = (props: LoginProps) => {}
+function Login() {
+  const login = async (data: LoginSignupFormData) => {
+    try {
+      const res = await axios.post('http://localhost:8080/auth/login/', data)
+      const { userData, token } = res.data.receivedData
+      localStorage.setItem('accessToken', token)
+      console.log(userData)
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error)
+      }
+    }
+  }
+
+  return (
+    <div className="login-form-wrapper w-full flex justify-center items-center">
+      <LoginSignupForm
+        formType="login"
+        formLabel="Login"
+        email={true}
+        password={true}
+        abortButtonLabel="Cancel"
+        approveButtonLabel="Login"
+        callBackDataFunction={login}
+      />
+    </div>
+  )
+}
 export default Login
--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,4 +1,24 @@
-interface RegisterProps {}
+import axios from 'axios'
+import LoginSignupForm from '../components/LoginSignupForm'
+import LoginSignupFormData from '../interfaces/LoginSignupFormData'
 
-const Register = (props: RegisterProps) => {}
+function Register() {
+  const register = async (data: LoginSignupFormData) => {
+    await axios.post('http://localhost:8080/auth/register', data)
+  }
+  return (
+    <div className="login-form-wrapper w-full flex justify-center items-center">
+      <LoginSignupForm
+        formType="signup"
+        formLabel="Register"
+        email={true}
+        password={true}
+        userName={true}
+        abortButtonLabel="Cancel"
+        approveButtonLabel="Register"
+        callBackDataFunction={register}
+      />
+    </div>
+  )
+}
 export default Register
