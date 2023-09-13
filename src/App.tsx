c2a73df38e2b8c8519b53c2b3fee5882229c372f
ZingerEngineer <ramyisez@gmail.com>
2023-09-13 05:11:10 +0300

    Changed arrow function to normal, views to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './views/Home'
-import Login from './views/Login'
-import Register from './views/Register'
-import ForgotPassword from './views/ForgotPassword'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect, useMemo } from 'react'
 import { UserData } from './interfaces/userData'
+import CreateNewPassword from './pages/CreateNewPassword'
 
 export const ThemeContext = createContext('light')
 export const userDataContext = createContext<{
   userData: UserData | null
   setUserData: React.Dispatch<React.SetStateAction<UserData | null>>
 } | null>(null)
-const App = () => {
+function App() {
   const theme = useContext(ThemeContext)
   const [userData, setUserData] = useState<UserData | null>(null)
   return (
@@ -42,6 +43,10 @@ const App = () => {
             <Route
               path="/forgot-password"
               element={<ForgotPassword />}
+            ></Route>
+            <Route
+              path="/create-new-password/:frgt"
+              element={<CreateNewPassword />}
             ></Route>
           </Routes>
         </userDataContext.Provider>
