3ae8ed758bf8bd0322eecc9ad7c83dc17277ffee
ZingerEngineer <ramyisez@gmail.com>
2023-09-28 18:48:31 +0300

    moved authgaurd function from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,29 +5,6 @@ import { useLocalStorage } from '../hooks/useLocalStorage'
 import NavBar from '../components/NavBar'
 
 const Home = () => {
-  const userState = useContext(userDataContext)
-  const { key } = useLocalStorage('access_token')
-
-  const authValidation = useCallback(async () => {
-    if (!key) return
-    try {
-      const res = await axios.post(
-        'http://localhost:8080/auth/validate-access-token',
-        null,
-        {
-          headers: {
-            authorization: key
-          }
-        }
-      )
-      userState?.setUserData(res.data.userData)
-    } catch (error) {
-      return error
-    }
-  }, [key, userState])
-  useLayoutEffect(() => {
-    authValidation()
-  }, [key, authValidation])
   return (
     <div className="home-wrapper w-full h-full">
       <NavBar />
